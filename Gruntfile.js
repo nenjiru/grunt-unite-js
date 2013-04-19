25f9cae9a8c266739cd9ae7cd754840f0724222c
minoru nakano <nakano-minoru@kayac.com>
2013-04-19 12:00:16 +0900

    example code

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,6 +30,6 @@ module.exports = function(grunt)
         }
     });
 
-    grunt.loadTasks('tasks');
-    //grunt.loadNpmTasks('grunt-unite-js');
+    //grunt.loadTasks('tasks');
+    grunt.loadNpmTasks('grunt-unite-js');
 };
